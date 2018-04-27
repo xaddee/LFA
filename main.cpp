834772bd5e62da57035f9f05094c5bab2e704d66
xaddee <32718823+xaddee@users.noreply.github.com>
2018-04-27 15:36:41 +0300

    Fix buggs
    Function was not working on all cases

--- main.cpp
+++ main.cpp
@@ -16,18 +16,19 @@
  */
 
 
-bool CheckWord(char *word, Road **roads ,Symbol start, int current_letter_index) {
-    if (current_letter_index == strlen(word) && start.showFinal()) return true;
+bool CheckWord(char* word, Road **roads ,Symbol start, int current_letter_index)
+{
+    if (current_letter_index == strlen(word)  && start.showFinal()) return true;
 
-    if (current_letter_index == strlen(word) && !start.showFinal()) return false;
+    if (current_letter_index == strlen(word)  && !start.showFinal()) return false;
 
     if (current_letter_index <= strlen(word) && start.showNumberOfRoads() == 0) return false;
 
-    for (int i = 0; i < start.showNumberOfRoads(); i++) {
-        if (roads[start.showIndex()][i].showLetter() == word[current_letter_index])
-            if (CheckWord(word, roads, roads[start.showIndex()][i].showNextSymbol(),  current_letter_index + 1)) return true;
+    for (int i = 0; i < start.showNumberOfRoads(); i++)
 
-    }
+        if (roads[start.showIndex()][i].showLetter() == word[current_letter_index])
+
+               if (CheckWord(word, roads, roads[start.showIndex()][i].showNextSymbol(), current_letter_index + 1)) return true;
 
     return false;
 }
@@ -40,10 +41,14 @@ int main()
 
     unsigned int number_of_symbols;
     f >> number_of_symbols;
+    int number_of_words;
+    f >> number_of_words;
 
-    char word[21];
-    f >> word;
+    char words[number_of_words][21];
 
+    for (int word_index = 0; word_index < number_of_words; word_index++) {
+        f >> words[word_index];
+    }
     Road **roads;
 
     roads = new Road *[number_of_symbols];
@@ -52,7 +57,7 @@ int main()
     symbols.resize(number_of_symbols);
 
 
-    for(int index = 0; index < number_of_symbols; index++)
+    for(int symbol_index = 0; symbol_index < number_of_symbols; symbol_index++)
     {
 
         int number_of_roads = 0;
@@ -61,16 +66,18 @@ int main()
         int final;
         f >> final;
 
-        if(final) symbols[index].setFinal(true);
-        else symbols[index].setFinal(false);
+        if(final) symbols[symbol_index].setFinal(true);
+        else symbols[symbol_index].setFinal(false);
 
-        symbols[index].setNumberOfRoads(number_of_roads);
-        roads[index] = new Road [number_of_roads];
+        symbols[symbol_index].setNumberOfRoads(number_of_roads);
+
+        roads[symbol_index] = new Road [number_of_roads];
+        symbols[symbol_index].setIndex(symbol_index);
     }
 
     for(int symbol_index = 0; symbol_index < number_of_symbols; symbol_index++)
     {
-        symbols[symbol_index].setIndex(symbol_index);
+
         for(int road_index = 0; road_index < symbols[symbol_index].showNumberOfRoads(); road_index++ )
         {
             char road_letter;
@@ -82,20 +89,30 @@ int main()
             f >> next_symbol;
 
             roads[symbol_index][road_index].setNextSymbol(symbols[next_symbol]);
+            Symbol next = roads[symbol_index][road_index].showNextSymbol();
 
         }
     }
 
-
-    if (word[0] == '#')
+    for(int word_index = 0; word_index < number_of_words; word_index++)
     {
-        std::cout << symbols[0].showFinal();
-        if (symbols[0].showFinal()) std::cout << "Cuvantul apartine gramaticii.";
-        else std::cout << "Cuvantul nu apartine gramaticii.";
+        if (words[word_index] == "#")
+        {
+            if (symbols[0].showFinal())
+                std::cout << "Cuvantul apartine gramaticii. \n";
+            else
+                std::cout << "Cuvantul nu apartine gramaticii. \n";
+        }
+        else
+        if (CheckWord(words[word_index], roads, symbols[0], 0))
+        {
+            std::cout << "Cuvantul apartine gramaticii.\n";
+        }
+        else
+        {
+            std::cout << "Cuvantul nu apartine gramaticii.\n";
+        }
     }
-    else if(CheckWord(word,roads,symbols[0],0)) std::cout << "Cuvantul apartine gramaticii.";
-    else std::cout << "Cuvantul nu apartine gramaticii.";
-
 
     return 0;
 }
